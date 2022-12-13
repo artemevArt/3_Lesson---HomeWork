46443c2ee3f488dde10a794ec0abe0fa66a9c659
Artem Artemev <artem@artemev.su>
2022-12-13 10:25:18 +0300

    1st task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("Введите 5-и значное число: ");
+string num1 = Console.ReadLine();
+char[] num2 = num1.Reverse().ToArray();
+//Console.WriteLine(num2);
+int a = int.Parse(num1);
+int b = int.Parse(num2);
+if (a == b)
+    Console.WriteLine("является палиндромом");
+else Console.WriteLine("не является палиндромом");
